06a3f2bf97b7405118a17bef46bcd76b16fd76dd
FadeAwayVoid <kapustin.gena2003@gmail.com>
2025-05-06 23:33:33 +0300

    Добавлен порт

--- main.py
+++ main.py
@@ -82,3 +82,6 @@ def set_webhook():
     success = bot.set_webhook(url=webhook_url)
     return f"Webhook установлен: {success}", 200
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
